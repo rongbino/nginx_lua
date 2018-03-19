a4a25cab5ccde71e6d8e0dbbb92c8c1aa286c532
rongbino <rongbino@163.com>
2018-03-19 10:18:57 +0800

    add two build script to git/mvn and run the jetty

---
+++ build/sess-build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+#GIT_REPOSITORY=http://github.com/passport/session-server.git
+GIT_HOME=/search/build/session.git-deploment
+BUILD_HOME=/search/build/session.build-deploment
+
+BRANCH=master
+
+if [ ! -d "/search/build" ];then
+        mkdir /search/build
+fi
+
+if [ ! -d "$GIT_HOME" ];then
+        mkdir $GIT_HOME
+fi
+
+if [ ! -d "$BUILD_HOME" ];then
+        mkdir $BUILD_HOME
+fi
+
+NUMBER=$#
+i=1
+while [[ $i -le $NUMBER ]]
+do                        #将数组a[i]赋值为$1,即取得到第一个参数并将值存入a[1]
+        if [[ $1  = "-b" ]]; then
+                ((i++))                       #数组后移一位,变为a[2]
+                shift
+                NUMBER=$NUMBER-1                    #使用shift命令将参数后移一位,即此时的$1为第二个参数
+                BRANCH=$1
+        fi
+        ((i++))                       #数组后移一位,变为a[2]
+        shift                     #使用shift命令将参数后移一位,即此时的$1为第二个参数
+done
+
+echo '部署分支:'${BRANCH}
+
+sleep 1
+
+# update git & copy to build_home
+rm -rf $GIT_HOME
+git clone $GIT_REPOSITORY $GIT_HOME
+cd $GIT_HOME
+git fetch
+git checkout -b $BRANCH origin/$BRANCH
+read old_hits <<< git show | head -1 | awk '{print $2}'
+rsync -az --delete $GIT_HOME/* $BUILD_HOME/
+
+# mvn build
+cd $BUILD_HOME
+MVN=/usr/local/maven/bin/mvn
+$MVN clean install -Dmaven.test.skip=true -Pprod -s /search/pre_deploy_script/deploment/settings.xml
+
+WORK_DIR=/search/jetty/webapps
+
+mv -f passport-session-web/target/passport-session-web.war $WORK_DIR/ROOT.war
+/usr/local/jetty/bin/jetty.sh restart
+
+rm -rf $GIT_HOME
+rm -rf $BUILD_HOME
